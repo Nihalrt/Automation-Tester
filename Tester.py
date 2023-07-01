53e9a0afaca3071b5329c73b425f7c299271dce9
Sai Nihal <109018562+Nihalrt@users.noreply.github.com>
2023-07-01 15:39:06 -0700

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -1,31 +1,41 @@
-from selenium import webdriver
 import unittest
 import HtmlTestRunner
+from selenium import webdriver
 
-class GoogleSearch(unittest.TestCase):
+class GoogleSearchTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome(executable_path='../drivers/chromedriver.exe')
+        cls.driver = webdriver.Chrome(executable_path='path/to/chromedriver')
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
-    def test_search_automationstepbystep(self):
-        self.driver.get("https://google.com")
-        self.driver.find_element_by_name("q").send_keys("Automation Step by Step")
-        self.driver.find_element_by_name("btnK").click()
+    def test_search_automation(self):
+        self.driver.get("https://www.google.com")
+        search_input = self.driver.find_element_by_name("q")
+        search_input.clear()
+        search_input.send_keys("Automation")
+        search_input.submit()
 
-    def test_search_Elon(self):
-        self.driver.get("https://google.com")
-        self.driver.find_element_by_name("q").send_keys("Elon Musk")
-        self.driver.find_element_by_name("btnK1").click()
+        # Assertion to check if the search results page contains the query
+        self.assertIn("Automation", self.driver.title)
+
+    def test_search_selenium(self):
+        self.driver.get("https://www.google.com")
+        search_input = self.driver.find_element_by_name("q")
+        search_input.clear()
+        search_input.send_keys("Selenium")
+        search_input.submit()
+
+        # Assertion to check if the search results page contains the query
+        self.assertIn("Selenium", self.driver.title)
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.close()
         cls.driver.quit()
-        print("Test Completed")
+        print("Test completed successfully.")
 
 
-if _name_ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/Administrator/PycharmProjects/Selenium/reports'))
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='path/to/reports'))
